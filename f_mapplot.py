e08406e9fecbb215dfde2f3a0951c59eb9d373dd
oscar-kogenhop <oscar.kogenhop@epcor.nl>
2024-12-02 09:40:30 +0100

    MapPlot now plots to images stored in ./data

--- f_mapplot.py
+++ f_mapplot.py
@@ -4,7 +4,7 @@ from f_turbine import TTurbine
 import numpy as np
 from scipy.interpolate import griddata
 from scipy.ndimage import gaussian_filter
-
+from PIL import Image
 
 class MapPlot:
     # properties
@@ -99,16 +99,77 @@ class TurbineMapPlot(MapPlot):
         self.tAx.set_ylabel('Corected massflow')
         self.tAx.set_xlabel('Pressure Ratio')
         
-        # Plot surge line
-        # self.tAx.plot(self.turbSlWcArrayValues, self.turbSlPRArrayValues[0], linewidth=1.0, linestyle='solid', color='red', label='Surge Line')
-        # self.ax.legend()
-        
         # Contours
         PR_grid, Wc_grid, Eta_grid = self.CreateEtaTopology(self.turbWcArrayValues,self.turbPRArrayValues,self.turbEtaArrayValues,False)
         CS = self.tAx.contour(Wc_grid,PR_grid,np.transpose(Eta_grid),10,colors='slategrey',alpha=0.3,levels = np.linspace(0.64, 0.84, 11)) 
         self.tAx.clabel(CS, fontsize=7, inline=True)
         self.tAx.contourf(Wc_grid,PR_grid,np.transpose(Eta_grid), 14 ,cmap='RdYlGn',alpha=0.3)
+        
+    def PlotOperatingCurve(self,wc_values, pr_values, eta_values):
+        # Pass operating data to plot in the map
+        if self.figTurbSplit is not None:
+            plt.figure('Split turbine map plot')
+            self.tAxSplitTop.plot(pr_values, eta_values, linewidth=1.5, linestyle='solid', color='navy')
+            self.tAxSplitBottom.plot(pr_values, wc_values, linewidth=1.5, linestyle='solid', color='navy')
+        if self.figTurbContour is not None:
+            plt.figure('Turbine map plot')
+            self.tAx.plot(pr_values, wc_values,  linewidth=1.5, linestyle='solid', color='navy')
+        
+    def PlotDesignPoint(self,wc_value, pr_value, eta_value):
+        if self.figTurbContour is not None:
+            plt.figure('Turbine map plot')
+            self.tAx.plot(pr_value, wc_value,  markersize=6.0, linestyle='none', marker='s', markeredgewidth=0.75, markerfacecolor='yellow', markeredgecolor='black')
+            
+    def PlotMap(self):
+        super().PlotMap()
+        # Create a figure with the name "testplot"
+        figTurbSplit = plt.figure(num="Split turbine map plot", figsize=(10,8))
+        # Add two subplots to the figure
+        ax1 = figTurbSplit.add_subplot(2, 1, 1)  # First subplot (2 rows, 1 column, position 1)
+        ax2 = figTurbSplit.add_subplot(2, 1, 2)  # Second subplot (2 rows, 1 column, position 2)
+        self.figTurbSplit = figTurbSplit
+        self.tAxSplitTop = ax1
+        self.tAxSplitBottom = ax2
+                
+        # Get the data arrays for plotting the map
+        self.GetMapData()
+       
+        self.figTurbSplit.suptitle((self.turbineObject).MapFileName)
+        
+        # Plot PR-Eta top subplot
+        for index, NcValue in enumerate(self.turbNcArrayValues): 
+            # self.tAxSplitTop.plot(self.turbWcArrayValues[index], self.turbEtaArrayValues[index], linewidth=0.5, linestyle='dashed', color='black', label=str(NcValue))
+            self.tAxSplitTop.plot(self.turbPRArrayValues[index], self.turbEtaArrayValues[index], linewidth=0.5, linestyle='dashed', color='black', label=str(NcValue))
+        self.tAxSplitTop.set_ylabel('Efficiency')
+                
+        # Plot Wc-PR top subplot
+        # for index, NcValue in enumerate(self.turbNcArrayValues): 
+        #     # self.cAxSplitBottom.plot(self.compWcArrayValues[index], self.compPRArrayValues[index], linewidth=0.25, linestyle='dashed', color='black', label=str(NcValue))
+        #     self.tAxSplitBottom.plot(self.turbPRArrayValues[index], self.turbWcArrayValues[index], linewidth=0.25, linestyle='dashed', color='black', label=str(NcValue))
+        self.tAxSplitBottom.set_ylabel('Corected massflow')
+        self.tAxSplitBottom.set_xlabel('Pressure Ratio')
+        
+        # Contours
+        PR_grid, Wc_grid, Eta_grid = self.CreateEtaTopology(self.turbWcArrayValues,self.turbPRArrayValues,self.turbEtaArrayValues,False)
+        CS = self.tAxSplitBottom.contour(Wc_grid,PR_grid,np.transpose(Eta_grid),10,colors='slategrey',alpha=0.3,levels = np.linspace(0.64, 0.84, 11)) 
+        self.tAxSplitBottom.clabel(CS, fontsize=7, inline=True)
+        self.tAxSplitBottom.contourf(Wc_grid,PR_grid,np.transpose(Eta_grid), 14 ,cmap='RdYlGn',alpha=0.3)
 
+    def ShowPlot(self):
+        if self.figTurbSplit is not None:
+            # self.figTurbSplit.show
+            self.figTurbSplit.savefig('./data/turbSplit.jpg')
+            # # Load an image
+            # imgTurbSplit = Image.open('./data/turbSplit.jpg')
+            # # Display the image
+            # imgTurbSplit.show()
+        if self.figTurbContour is not None:
+            # self.figTurbContour.show
+            self.figTurbContour.savefig('./data/turbContour.jpg')
+            #  # Load an image
+            # imgTurbContour = Image.open('./data/turbContour.jpg')
+            # # Display the image
+            # imgTurbContour.show()
     
 class CompressorMapPlot(MapPlot):
     # constructor
@@ -144,7 +205,7 @@ class CompressorMapPlot(MapPlot):
         figCompContour = plt.figure(num='Compressor map plot', figsize=(10,8))
         ax = figCompContour.gca()
         self.figCompContour = figCompContour
-        self.ax = ax
+        self.cAx = ax
         # Get the data arrays for plotting the map
         self.GetMapData()
         
@@ -152,28 +213,28 @@ class CompressorMapPlot(MapPlot):
 
         # Plot Wc-PR top subplot
         for index, NcValue in enumerate(self.compNcArrayValues): 
-            self.ax.plot(self.compWcArrayValues[index], self.compPRArrayValues[index], linewidth=0.25, linestyle='dashed', color='black', label=str(NcValue))
-        self.ax.set_xlabel('Corected massflow')
-        self.ax.set_ylabel('Pressure Ratio')
+            self.cAx.plot(self.compWcArrayValues[index], self.compPRArrayValues[index], linewidth=0.25, linestyle='dashed', color='black', label=str(NcValue))
+        self.cAx.set_xlabel('Corected massflow')
+        self.cAx.set_ylabel('Pressure Ratio')
         
         # Plot surge line
-        self.ax.plot(self.compSlWcArrayValues, self.compSlPRArrayValues[0], linewidth=1.0, linestyle='solid', color='red', label='Surge Line')
+        self.cAx.plot(self.compSlWcArrayValues, self.compSlPRArrayValues[0], linewidth=1.0, linestyle='solid', color='red', label='Surge Line')
         # self.ax.legend()
         
         # Contours
         Wc_grid, PR_grid, Eta_grid = self.CreateEtaTopology(self.compWcArrayValues,self.compPRArrayValues,self.compEtaArrayValues,False)
-        CS = self.ax.contour(Wc_grid,PR_grid,Eta_grid,10,colors='slategrey',alpha=0.3,levels = np.linspace(0.64, 0.84, 11)) 
-        self.ax.clabel(CS, fontsize=7, inline=True)
-        self.ax.contourf(Wc_grid,PR_grid,Eta_grid, 14 ,cmap='RdYlGn',alpha=0.3)
+        CS = self.cAx.contour(Wc_grid,PR_grid,Eta_grid,10,colors='slategrey',alpha=0.3,levels = np.linspace(0.64, 0.84, 11)) 
+        self.cAx.clabel(CS, fontsize=7, inline=True)
+        self.cAx.contourf(Wc_grid,PR_grid,Eta_grid, 14 ,cmap='RdYlGn',alpha=0.3)
         
     def PlotMap(self):
         super().PlotMap()
         # Create a figure with the name "testplot"
-        figCompContour = plt.figure(num="Split compressor map plot", figsize=(10,8))
+        figCompSplit = plt.figure(num="Split compressor map plot", figsize=(10,8))
         # Add two subplots to the figure
-        ax1 = figCompContour.add_subplot(2, 1, 1)  # First subplot (2 rows, 1 column, position 1)
-        ax2 = figCompContour.add_subplot(2, 1, 2)  # Second subplot (2 rows, 1 column, position 2)
-        self.figCompSplit = figCompContour
+        ax1 = figCompSplit.add_subplot(2, 1, 1)  # First subplot (2 rows, 1 column, position 1)
+        ax2 = figCompSplit.add_subplot(2, 1, 2)  # Second subplot (2 rows, 1 column, position 2)
+        self.figCompSplit = figCompSplit
         self.cAxSplitTop = ax1
         self.cAxSplitBottom = ax2
                 
@@ -205,8 +266,22 @@ class CompressorMapPlot(MapPlot):
         # self.ax2.legend()
 
     def ShowPlot(self):
-        plt.show()
-    
+        # plt.show()
+        if self.figCompSplit is not None:
+            # self.figCompSplit.show()
+            self.figCompSplit.savefig('./data/compSplit.jpg')
+            # # Load an image
+            # imgCompSplit = Image.open('./data/compSplit.jpg')
+            # # Display the image
+            # imgCompSplit.show()
+        if self.figCompContour is not None:
+            # self.figCompContour.show() 
+            self.figCompContour.savefig('./data/compContour.jpg')
+            #  # Load an image
+            # imgCompContour = Image.open('./data/compContour.jpg')
+            # # Display the image
+            # imgCompContour.show()
+               
     def PlotOperatingCurve(self,wc_values, pr_values, eta_values):
         # Pass operating data to plot in the map
         if self.figCompSplit is not None:
@@ -215,8 +290,7 @@ class CompressorMapPlot(MapPlot):
             self.cAxSplitBottom.plot(wc_values, pr_values, linewidth=1.5, linestyle='solid', color='navy')
         if self.figCompContour is not None:
             plt.figure('Compressor map plot')
-            self.ax.plot(wc_values, pr_values,  linewidth=1.5, linestyle='solid', color='navy')
-        
+            self.cAx.plot(wc_values, pr_values,  linewidth=1.5, linestyle='solid', color='navy')
         
     def PlotDesignPoint(self,wc_value, pr_value, eta_value):
         if self.figCompSplit is not None:
@@ -225,7 +299,7 @@ class CompressorMapPlot(MapPlot):
             self.cAxSplitBottom.plot(wc_value, pr_value, markersize=6.0, linestyle='none', marker='s', markeredgewidth=0.75, markerfacecolor='yellow', markeredgecolor='black')
         if self.figCompContour is not None:
             plt.figure('Compressor map plot')
-            self.ax.plot(wc_value, pr_value,  markersize=6.0, linestyle='none', marker='s', markeredgewidth=0.75, markerfacecolor='yellow', markeredgecolor='black')
+            self.cAx.plot(wc_value, pr_value,  markersize=6.0, linestyle='none', marker='s', markeredgewidth=0.75, markerfacecolor='yellow', markeredgecolor='black')
         
         
 # test program start
@@ -239,10 +313,11 @@ if __name__ == "__main__":
     aCompressor.SFmap_Eta = 0.948276
     aCompressor.SFmap_PR = 1.051659
     aCompressor.SFmap_Nc = 16539.99909
-    aTurbine.SFmap_Wc = 0.30258720860030913
-    aTurbine.SFmap_Eta = 0.9658696778385591
-    aTurbine.SFmap_PR = 0.6465586472480385
-    aTurbine.SFmap_Nc = 7986.39865810925
+    
+    aTurbine.SFmap_Wc = 0.329704
+    aTurbine.SFmap_Eta = 0.944733
+    aTurbine.SFmap_PR = 1.000753
+    aTurbine.SFmap_Nc = 8276.062745
     
     # Create turbo map objects
     compMap = CompressorMapPlot("compmap.map", aCompressor)
@@ -250,6 +325,7 @@ if __name__ == "__main__":
 
     # Plot the basic map curves
     compMap.PlotMap()
+    turbMap.PlotMap()
     
     # Plot contours
     compMap.ContourPlotMap()
@@ -260,10 +336,13 @@ if __name__ == "__main__":
     df = pd.read_csv('./data/TJET.csv')
     
     # Plot design point
-    compMap.PlotDesignPoint(df['Wc'][0],df['PR'][0],df['Eta'][0])
+    # Compressor data: Wc2 [kg/s],PR_c [-],Nc_c [%],Eta_c [-]
+    compMap.PlotDesignPoint(df['Wc2 [kg/s]'][0],df['PR_c [-]'][0],df['Eta_c [-]'][0])
+    turbMap.PlotDesignPoint(df['Wc4 [kg/s]'][0],df['PR_t [-]'][0],df['Eta_t [-]'][0])
     
     # Plot steady state series
-    compMap.PlotOperatingCurve(df['Wc'],df['PR'],df['Eta'])
+    compMap.PlotOperatingCurve(df['Wc2 [kg/s]'],df['PR_c [-]'],df['Eta_c [-]'])
+    turbMap.PlotOperatingCurve(df['Wc4 [kg/s]'],df['PR_t [-]'],df['Eta_t [-]'])
     
     # Finally, show created plot
     compMap.ShowPlot()
