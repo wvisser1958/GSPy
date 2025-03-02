12d2eedf7ec6a035eebb9ca8d7e097315d0008cf
wvisser1958 <wvisser@xs4all.nl>
2025-03-02 20:32:11 +0100

    ppt almost  complete

--- f_gaspath.py
+++ f_gaspath.py
@@ -1,40 +1,39 @@
 import numpy as np
 import cantera as ct
-from f_BaseComponent import TComponent 
+from f_BaseComponent import TComponent
 import f_global as fg
 import f_system as fsys
 
-class TGaspath(TComponent):        
+class TGaspath(TComponent):
     def __init__(self, name, MapFileName, stationin, stationout):    # Constructor of the class
-        super().__init__(name, MapFileName)   
+        super().__init__(name, MapFileName)
         self.stationin = stationin
         self.stationout = stationout
         # set design properties to None, if still None in PrintPerformance,
-        # then not assigned anywhere so no need to Print/output.         
+        # then not assigned anywhere so no need to Print/output.
         self.GasIn = None
         self.GasOut = None
         self.Wc = None
         self.PRdes = 1
         self.PR = None
-            
-    def Run(self, Mode, PointTime):    
-        # self.GasIn = ct.Quantity(GasIn.phase, mass = GasIn.mass) 
-        self.GasIn = fsys.gaspath_conditions[self.stationin] 
-        
+
+    def Run(self, Mode, PointTime):
+        self.GasIn = fsys.gaspath_conditions[self.stationin]
+
         if Mode == 'DP':
             # create GasInDes, GasOut cantera Quantity (GasIn already created)
-            self.GasInDes = ct.Quantity(self.GasIn.phase, mass = self.GasIn.mass) 
-            self.GasOut = ct.Quantity(self.GasIn.phase, mass = self.GasIn.mass) 
+            self.GasInDes = ct.Quantity(self.GasIn.phase, mass = self.GasIn.mass)
+            self.GasOut = ct.Quantity(self.GasIn.phase, mass = self.GasIn.mass)
             self.Wdes = self.GasInDes.mass
-            self.Wcdes = self.Wdes * fg.GetFlowCorrectionFactor(self.GasInDes)        
+            self.Wcdes = self.Wdes * fg.GetFlowCorrectionFactor(self.GasInDes)
             self.Wc = self.Wcdes
         else:
             self.GasOut.TPY = self.GasIn.TPY
             self.GasOut.mass = self.GasIn.mass
-            
-        fsys.gaspath_conditions[self.stationout] = self.GasOut       
-        return self.GasOut    
-    
+
+        fsys.gaspath_conditions[self.stationout] = self.GasOut
+        return self.GasOut
+
     def PrintPerformance(self, Mode, PointTime):
         super().PrintPerformance(Mode, PointTime)
         print(f"\tInlet conditions:")
@@ -56,15 +55,15 @@ class TGaspath(TComponent):
 
     def GetOutputTableColumnNames(self):
         return [f"W{self.stationin}", f"Wc{self.stationin}", f"T{self.stationin}", f"P{self.stationin}", "PR_"+self.name]
-         
+
     def AddOutputToTable(self, Mode, rownr):
         for columnname in self.GetOutputTableColumnNames():
-            # fg.OutputTable.loc[rownr, columnname] = getattr(self, columnname) 
-            fsys.OutputTable.loc[rownr, f"W{self.stationin}"]  = self.GasIn.mass 
-            fsys.OutputTable.loc[rownr, f"Wc{self.stationin}"] = self.Wc 
-            fsys.OutputTable.loc[rownr, f"T{self.stationin}"]  = self.GasIn.T 
-            fsys.OutputTable.loc[rownr, f"P{self.stationin}"]  = self.GasIn.P 
+            # fg.OutputTable.loc[rownr, columnname] = getattr(self, columnname)
+            fsys.OutputTable.loc[rownr, f"W{self.stationin}"]  = self.GasIn.mass
+            fsys.OutputTable.loc[rownr, f"Wc{self.stationin}"] = self.Wc
+            fsys.OutputTable.loc[rownr, f"T{self.stationin}"]  = self.GasIn.T
+            fsys.OutputTable.loc[rownr, f"P{self.stationin}"]  = self.GasIn.P
             if self.PR != None:
-                fsys.OutputTable.loc[rownr, "PR_"+self.name] = self.PR 
-          
-            
+                fsys.OutputTable.loc[rownr, "PR_"+self.name] = self.PR
+
+
--- f_inlet.py
+++ f_inlet.py
@@ -2,40 +2,35 @@ import numpy as np
 import cantera as ct
 import f_global as fg
 import f_system as fsys
-from f_gaspath import TGaspath as gaspath
+from f_gaspath import TGaspath
 
-class TInlet(gaspath):
-    def __init__(self, name, MapFileName, stationin, stationout, Wdes, PRdes):    # Constructor of the class
-        super().__init__(name, MapFileName, stationin, stationout)    
+class TInlet(TGaspath):
+    def __init__(self, name, MapFileName, stationin, stationout, Wdes, PRdes):  # Constructor of the class
+        super().__init__(name, MapFileName, stationin, stationout)
         self.Wdes = Wdes
-        self.PRdes = PRdes        
+        self.PRdes = PRdes
 
-    def Run(self, Mode, PointTime):       
-        # self.GasIn = ct.Quantity(fsys.gaspath_conditions[self.stationin].phase, mass = self.Wdes) 
+    def Run(self, Mode, PointTime):
         if Mode == 'DP':
             fsys.gaspath_conditions[self.stationin].mass = self.Wdes
         super().Run(Mode, PointTime)
         self.GasIn.TP = self.GasIn.T, self.GasIn.P
         if Mode == 'DP':
-            self.GasIn.mass = self.Wdes                     
+            self.GasIn.mass = self.Wdes
             self.wcdes = self.GasIn.mass * fg.GetFlowCorrectionFactor(self.GasIn)
             self.wc = self.wcdes
-            self.PR = self.PRdes  
-
-            # add state for corrected inlet flow wc
-            # question: why corrected? : more stable... state staying closer to 1 at high altitude
+            self.PR = self.PRdes
             fsys.states = np.append(fsys.states, 1)
-            self.istate_wc = fsys.states.size-1           
+            self.istate_wc = fsys.states.size-1   # add state for corrected inlet flow wc more stable... state staying closer to 1 at high altitude
         else:
             self.wc = fsys.states[self.istate_wc] * self.wcdes
-            self.GasIn.mass = self.wc / fg.GetFlowCorrectionFactor(self.GasIn)                     
-            self.GasOut.TP = self.GasIn.T, self.GasIn.P * self.PRdes  
+            self.GasIn.mass = self.wc / fg.GetFlowCorrectionFactor(self.GasIn)
+            self.GasOut.TP = self.GasIn.T, self.GasIn.P * self.PRdes
             # this inlet has constant PR, no OD PR yet (use manual input in code here, or make PR, Ram recovery map)
-            self.PR = self.PRdes  
-
-        self.GasOut.TP = self.GasIn.T, self.GasIn.P * self.PR              
-        self.GasOut.mass = self.GasIn.mass            
-        self.RD = self.GasIn.mass * fsys.Ambient.V    
+            self.PR = self.PRdes
+        self.GasOut.TP = self.GasIn.T, self.GasIn.P * self.PR
+        self.GasOut.mass = self.GasIn.mass
+        self.RD = self.GasIn.mass * fsys.Ambient.V
         # add ram drag to system level ram drag (note that multiple inlets may exist)
-        fsys.RD = fsys.RD + self.RD        
+        fsys.RD = fsys.RD + self.RD
         return self.GasOut
